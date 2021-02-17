15286c2ba6701e383ee64ea415391fa16ced2293
Jaron Johnson <jjohnson@instructure.com>
2021-02-17 11:27:06 -0700

    set up some logs to know the process

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    console.log("Pulling all links from site");
     await page.goto('https://www.gocomics.com/comics/a-to-z');
     const comics = await page.evaluate(() => {
         const comicEle = document.getElementsByClassName('gc-blended-link gc-blended-link--primary col-12 col-sm-6 col-lg-4');
@@ -19,9 +20,9 @@ const puppeteer = require('puppeteer');
         });
         return comicArray;
     })
-    console.log("🚀 ~ file: main.js ~ line 22 ~ comics ~ comics", comics)
     const curatedArr = ["The Argyle Sweater", "Calvin and Hobbes", "Close to Home", "DeFlocked", "Dilbert Classics", "F Minus", "FoxTrot", "Garfield", "Get Fuzzy", "In the Bleachers", "Lio", "Non Sequitur", "Pearls Before Swine", "Perry Bible Fellowship", "Pooch Cafe", "Wallace the Brave"]
     const chosenComics = [];
+    console.log("Grabbing curated list of comics");
     for (let i = 0; i < curatedArr.length; i++) {
         const comicName = curatedArr[i];
         console.log(`Grabbing ${i + 1} of ${curatedArr.length} comics`);
@@ -37,6 +38,6 @@ const puppeteer = require('puppeteer');
             chosenComics.push(comics[comicName]);
         }
     }
-    console.log("🚀 ~ file: main.js ~ line 12 ~ links", chosenComics)
+    console.log("🚀 ~ file: main.js ~ line 39 ~ chosenComics", chosenComics)
     await browser.close();
 })();
